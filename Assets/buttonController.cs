e4abda3050d5355a8ccdca6c817be0e02fa7401d
Zeynep <79641886+AlZenn@users.noreply.github.com>
2024-05-14 00:41:07 +0300

    i have a problem project
    omg II proje

---
+++ Assets/buttonController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonController : MonoBehaviour
+{
+
+public void gameContunie()
+    {
+        Time.timeScale = 1f;
+    }public void gamefreeze()
+    {
+        Time.timeScale = 0f;
+    }
+}
